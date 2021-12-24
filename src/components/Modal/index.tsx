11592db1d78a2227d72065bd558231be6d00fab1
何宇 <664475324@qq.com>
2021-12-24 16:46:34 +0800

    null

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -25,7 +25,6 @@ function Modal(props: MemoModal) {
 			root?.append(container)
 			return () => {
 				container.remove()
-				ReactDOM.unmountComponentAtNode(container)
 			}
 		}
 	}, [props.visible])
